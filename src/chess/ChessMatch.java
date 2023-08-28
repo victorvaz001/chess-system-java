dee2c230b171c8d2542d7be4549ab19e29a0fde7
Victor Vaz <victorvaz001@gmail.com>
2023-08-28 15:36:51 -0300

    Chess layer and printing the board

---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,23 @@
+package chess;
+
+import boardgame.Board;
+
+public class ChessMatch {
+	
+	private Board board;
+	
+	public ChessMatch() {
+		board = new Board(8,8); /*Tamanho do tabuleiro de Xadrez*/
+	}
+	
+	public ChessPiece[][] getPieces(){ /*Retorna uma Matriz de peças*/
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		for(int i = 0; i<board.getRows(); i++) {
+			for(int j = 0; j<board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) board.piece(i, j); /*Interpreta como uma peça de xadrez*/
+			}
+		}
+		return mat;
+	}
+		
+}
