e77c9b41ea856d9f03b718b627c9b2dfcbb1cb6b
Victor Vaz <victorvaz001@gmail.com>
2023-08-29 15:49:09 -0300

    Implementing possible moves of Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		ValidateSourcePosition(source); /*validar a posição de origem*/
+		validateTargetPosition(source, target); /*validar a posição de destino*/
 		Piece capturedPiece = makeMove(source, target); /*makeMove-> realiza o movimento da peça */
 		return (ChessPiece)capturedPiece;
 	}
@@ -47,6 +48,17 @@ public class ChessMatch {
 		}
 		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
+		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		/*se pra peça de origem, 
+		a posição de destino não e 
+		um movimento possivel, então 
+		não posso mecher essa peça pra la  */
+		if (!board.piece(source).possibleMove(target)) { 
+			throw new ChessException("The chosen piece can't move to target position");
+			
 		}
 	}
 	
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 	
@@ -16,5 +17,9 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 	
-	
+	/*Verifica se existe uma peça adversaria na 'position'*/
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position); //pegando a peça da posição
+		return p != null && p.getColor() != color; // se a cor da peça da posição e a cor diferente da cor da minha peça
+	}
 }
